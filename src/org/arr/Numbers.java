232642188846fe8c4b0be14b2b2fbd558e36f201
sharath7787 <sharathmatrix77@gmail.com>
2022-05-22 11:48:35 +0530

    mayproject

---
+++ src/org/arr/Numbers.java
@@ -0,0 +1,28 @@
+package org.arr;
+
+public class Numbers {
+	
+
+	public static void main(String[] args) {
+		int a[]=new int[10];
+	
+				a[0]=1;
+		a[1]=2;
+		a[2]=3;
+		a[3]=4;
+		a[4]=5;
+		a[5]=6;
+		a[6]=7;
+		a[7]=8;
+		a[8]=9;
+		a[9]=10;
+		
+		for(int i=0;i<a.length;i++) 
+	
+	System.out.println(a[i]);
+		
+	
+	}
+	
+
+}
